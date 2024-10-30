e91836847a7ad179a2eefb9e402ce6024195c573
aiire <104533992+aiire@users.noreply.github.com>
2024-10-30 08:39:28 -0400

    Create version4.py

---
+++ version4.py
@@ -0,0 +1,47 @@
+import random 
+
+magic_number = random.randint(1, 10)
+
+print("Welcome to the Guessing Game!")
+print()
+
+MAX_TRIES = 5
+tries_left = MAX_TRIES
+guesses = []
+
+print(f"I'm thinking of a number between 1 and 10, and I'm giving you {tries_left} attempts to guess it!")
+
+win = False
+
+while tries_left > 0:
+    if len(guesses) > 0:
+        print(f"You've already guessed: \n{guesses}")
+
+    guess = int(input("Enter your guess: "))
+
+    if guess in guesses:
+        print("You already guessed that number.")
+        continue
+
+    guesses.append(guess)
+
+    if guess == magic_number:
+        win = True
+        break
+    
+    tries_left -= 1
+    
+    if tries_left == 1:
+        print("Wrong again!")
+        break
+    else:
+        print(f"Incorrect. You have {tries_left} {"tries" if tries_left != 1 else "try"} left.")
+        print()
+
+if win:
+    print("You win!")
+    print(f"It took you {MAX_TRIES - tries_left} out of {MAX_TRIES} attempts to guess right!")
+else:
+    print("Better luck next time...")
+    print("You lose!")
+    print(f"The number was {magic_number}!")
